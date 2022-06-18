7c85b471d527ed43aebe9ec8d7ab91f8e7294d23
bjaan <42038171+bjaan@users.noreply.github.com>
2022-06-18 17:27:32 +0200

    added [INPARENT[ [INROOT] [OUTROOT] & fixed some typos

--- README.md
+++ README.md
@@ -32,19 +32,27 @@ This other example will copy the source files that are missing in the folder of 
 
 `easybatchprocess "C:\Temp\original" *.png "C:\Temp\missing" "cmd.exe /c if not exist ""C:\Temp\processed\[INFILE]"" copy [IN] [OUT]"`
 
+This is example is handy to copy a number of files sitting a subfolder, like subtitle files where the parent folder is match the video file:
+
+`easybatchprocess.bat C:\Temp\Subs 2_English.srt C:\Temp\Subs2 "copy [IN] ""[OUTROOT]\[INPARENT].en.srt"""`
+
 ## Full usage ##
 
 ```
-easybatchprocess.bat folder_to_search file_types output_path command_to_run
+easybatchprocess.bat folder_to_search files_to_search output_path command_to_run
   folder_to_search: search for files recursively trough this folder: e.g. c:\Temp\Input
-  file_types: search for filetypes e.g. *.* or *.mkv
+  files_to_search: search for file types e.g. *.* or *.mkv
+                or specific names or patterns 2_English.srt or *English.srt
   output_path: output path for files found: e.g. C:\Temp\Output
   command_to_run: template command to execute for each file:
      tokens that are replaced: e.g. when file found is C:\Temp\Input\Season1\video1.mkv
              [IN] path and file name found: e.g. "C:\Temp\Input\Season1\video1.mkv"
-             [OUT] fully qualifed output file: output_path + relative path to the folder_to_search + filename: e.g. "C:\Temp\Output\Season1\video1.mkv"
+             [OUT] fully qualified output file: output_path + relative path to the folder_to_search + filename: e.g. "C:\Temp\Output\Season1\video1.mkv"
              [INPATH] absolute path where the file is found, the full path where the file is found: e.g. C:\Temp\Input\Season1\
+             [INPARENT] parent folder name of file that is found: e.g. Season1
              [OUTPATH] path where the output file created: output_path + relative path to the folder_to_search: e.g.  C:\Temp\Output\Season1\
              [INFILE] file name found: e.g. video1.mkv
+             [INROOT] same as folder_to_search
+             [OUTROOT] same as output_path
 ```
 
--- easybatchprocess.bat
+++ easybatchprocess.bat
@@ -9,8 +9,8 @@ if "%~4" == "" goto usage
 rem path tilde tricks see https://ss64.com/nt/syntax-args.html
 
 set folder_to_search=%~f1
-set file_types=%~2
-set file_types_2=%~2
+set files_to_search=%~2
+set files_to_search_2=%~2
 set output_path=%~f3
 set template_command=%~4
 set template_command=%template_command:""="%
@@ -25,12 +25,12 @@ echo Searching...
 title Searching...
 
 :processtypes
-FOR /f "tokens=1* delims=;" %%a IN ("%file_types%") DO IF "%%a" NEQ "" (
-  SET file_types=%%b
+FOR /f "tokens=1* delims=;" %%a IN ("%files_to_search%") DO IF "%%a" NEQ "" (
+  SET files_to_search=%%b
   CALL :processtype %%a
 )
 
-IF "%file_types%" NEQ "" goto :processtypes
+IF "%files_to_search%" NEQ "" goto :processtypes
 
 goto processfiles
 
@@ -51,12 +51,12 @@ ECHO Total files found: !ID! - Starting batch...
 set /a C=1
 
 :processfiletypes
-FOR /f "tokens=1* delims=;" %%a IN ("%file_types_2%") DO IF "%%a" NEQ "" (
-  SET file_types_2=%%b
+FOR /f "tokens=1* delims=;" %%a IN ("%files_to_search_2%") DO IF "%%a" NEQ "" (
+  SET files_to_search_2=%%b
   CALL :processfiletype %%a
 )
 
-IF "%file_types_2%" NEQ "" goto :processfiletypes
+IF "%files_to_search_2%" NEQ "" goto :processfiletypes
 
 echo Finished.
 title Finished.
@@ -66,8 +66,12 @@ goto end
 FOR /f "delims=" %%f IN ('dir /b /s "%folder_to_search%\%1"') DO (
   set fullpath=%%~ff
   set folder=%%~dpf
+  set parentfolder=%%~dpf
+  set parentfolder=!parentfolder:~0,-1!
+  for %%P in (!parentfolder!) do set parentfolder=%%~nxP
   set FileName="!fullpath!"
   set FilePath="!folder!"
+  set ParentFolderName=!parentfolder!
   set OutputName="%output_path%\!fullpath:~%folder_to_search_len%!"
   set OutputPath="%output_path%\!folder:~%folder_to_search_len%!"
   set FileNameOnly=%%~nxf
@@ -75,9 +79,12 @@ FOR /f "delims=" %%f IN ('dir /b /s "%folder_to_search%\%1"') DO (
   set cmd=!template_command!
   call set "cmd=%%cmd:[IN]=!FileName!%%"
   call set "cmd=%%cmd:[INPATH]=!FilePath!%%"
+  call set "cmd=%%cmd:[INPARENT]=!ParentFolderName!%%"
   call set "cmd=%%cmd:[OUT]=!OutputName!%%"
   call set "cmd=%%cmd:[OUTPATH]=!OutputName!%%"
   call set "cmd=%%cmd:[INFILE]=!FileNameOnly!%%"
+  call set "cmd=%%cmd:[INROOT]=!folder_to_search!%%"
+  call set "cmd=%%cmd:[OUTROOT]=!output_path!%%"
   echo Command: !cmd!
   call set "output_folder=!OutputPath!"
   if not exist !output_folder! mkdir !output_folder!
@@ -88,16 +95,20 @@ goto:eof
 
 :usage
 echo Generate a batch processing batch file
-echo easybatchprocess folder_to_search file_types output_path command_to_run
+echo easybatchprocess folder_to_search files_to_search output_path command_to_run
 echo   folder_to_search: search for files recursively trough this folder: e.g. c:\Temp
-echo   file_types: search for filetypes e.g. *.* or *.mkv
+echo   files_to_search: search for file types e.g. *.* or *.mkv 
+echo                 or specific names or patterns 2_English.srt or *English.srt
 echo   output_path: output path for files found: e.g. C:\Temp\Output
 echo   command_to_run: template command to execute for each file:
 echo      tokens that are replaced: e.g. when file found is C:\Temp\Input\Season1\video1.mkv
 echo              [IN] path and file name found: e.g. "C:\Temp\Input\Season1\video1.mkv"
-echo              [OUT] fully qualifed output file: output_path + relative path to the folder_to_search + filename: e.g. "C:\Temp\Output\Season1\video1.mkv"
+echo              [OUT] fully qualified output file: output_path + relative path to the folder_to_search + filename: e.g. "C:\Temp\Output\Season1\video1.mkv"
 echo              [INPATH] absolute path where the file is found, the full path where the file is found: e.g. C:\Temp\Input\Season1\
+echo              [INPARENT] parent folder name of file that is found: e.g. Season1
 echo              [OUTPATH] path where the output file created: output_path + relative path to the folder_to_search: e.g.  C:\Temp\Output\Season1\
 echo              [INFILE] file name found: e.g. video1.mkv
+echo              [INROOT] same as folder_to_search
+echo              [OUTROOT] same as output_path
 
 :end
